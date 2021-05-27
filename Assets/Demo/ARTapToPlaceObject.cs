99ff162c8f55054824416cb60d64ef4608692583
Pham Ngoc Son <43614413+pns1681999@users.noreply.github.com>
2021-05-27 21:25:06 +0700

    tap to place

--- Assets/Demo/ARTapToPlaceObject.cs
+++ Assets/Demo/ARTapToPlaceObject.cs
@@ -2,28 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 using UnityEngine.Experimental.XR;
+using System;
 
 public class ARTapToPlaceObject : MonoBehaviour
 {
+    public GameObject placementIndicator;
     private ARSessionOrigin arOrigin;
+    private ARRaycastManager rayCastMgr;
     private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    private TrackableType trackableTypeMask = TrackableType.Planes;
     // Start is called before the first frame update
     void Start()
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
+        rayCastMgr = arOrigin.GetComponent<ARRaycastManager>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePlacementPose()
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+      if(placementPoseIsValid)
+      {
+        placementIndicator.SetActive(true);
+        placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+      }
+      else
+      {
+        placementIndicator.SetActive(false);
+      }
     }
 
     private void UpdatePlacementPose()
     {
       var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-      var hits = new List<
-      arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
+      var hits = new List<ARRaycastHit>();
+      // arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
+      rayCastMgr.Raycast(screenCenter, hits, trackableTypeMask);
+
+      placementPoseIsValid = hits.Count > 0;
+      if (placementPoseIsValid)
+      {
+        placementPose = hits[0].pose;
+      }
     }
 }
